c0421bfece52c63725fa77f09e5bb5f37a26d8dd
Devika223320 <72210804+Devika223320@users.noreply.github.com>
2020-10-01 20:11:24 +0530

    Update roots_quadratic.java

--- roots_quadratic.java
+++ roots_quadratic.java
@@ -8,19 +8,19 @@ public class roots_quadratic {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = input.nextInt();
-        int root = (b * b) - 4 * (a * c);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int z = input.nextInt();
+        int root = y*y - 4*x*z;
         double roots = sqrt(root);
-        if (root > 0) {
+        if (root>0) {
             System.out.println("Real and Distinct");
-            System.out.println(((-b - (int) roots) / (2 * a)) + " " + ((-b + (int) roots) / (2 * a)));
-        } else if (root < 0) {
+            System.out.println(((-y - (int) roots) / (2*x)) + " " + ((-y + (int) roots) / (2*x)));
+        } else if (root<0) {
             System.out.println("Imaginary");
         } else {
             System.out.println("Real and Equal");
-            System.out.println(((-b + (int) roots) / (2 * a)) + " " + ((-b - (int) roots) / (2 * a)));
+            System.out.println(((-y + (int) roots) / (2*x)) + " " + ((-y - (int) roots) / (2*x)));
         }
     }
 
